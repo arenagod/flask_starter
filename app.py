96024c9ebdbde97962939c884f032d1e5b54dff3
rain <xxx@xxx.com>
2020-04-04 21:06:11 +0800

    add content

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'Hello world!'
+
+    return render_template('index.html')
 
 
 
